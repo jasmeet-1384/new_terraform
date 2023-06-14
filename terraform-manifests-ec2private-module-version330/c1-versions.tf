6de79078433cd34c58c130587e3a01431415f48d
jass <your@email.com>
2023-06-14 12:30:00 +0530

    jass

--- terraform-manifests-ec2private-module-version330/c1-versions.tf
+++ terraform-manifests-ec2private-module-version330/c1-versions.tf
@@ -1,24 +1,25 @@
-# Terraform Block
 terraform {
-  required_version = ">= 1.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
+  backend "remote" {
+    organization = "hashicrop-learn-jass"
+    workspaces {
+      name = "new_terraform"
+    }
+  }
+
+  required_version = "~>1.4"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.16"
     }
-    null = {
+
+     null = {
       source = "hashicorp/null"
       version = "~> 3.0"
-    }        
+    }  
   }
 }
 
-# Provider Block
 provider "aws" {
-  region  = var.aws_region
-  profile = "default"
+  region = var.aws_region
 }
-/*
-Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
-$HOME/.aws/credentials
-*/
