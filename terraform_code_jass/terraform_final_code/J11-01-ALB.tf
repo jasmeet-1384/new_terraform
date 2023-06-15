c89bde8ae9f2ad20afb364965a805ec03f12b27f
jass <your@email.com>
2023-06-15 17:28:33 +0530

    test

---
+++ terraform_code_jass/terraform_final_code/J11-01-ALB.tf
@@ -0,0 +1,62 @@
+# Terraform AWS Application Load Balancer (ALB)
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "8.6.1"
+
+  name = "${local.name}-myALB-jass"
+
+  load_balancer_type = "application"
+
+  vpc_id = module.vpc.vpc_id
+  subnets = [
+    module.vpc.public_subnets[0],
+    module.vpc.public_subnets[1]
+  ]
+  security_groups = [module.loadbalancer_sg.security_group_id]
+
+  # Listeners
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  #target_Group
+  target_groups = [
+    {
+      name_prefix      = "pref-jass"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+
+      health_check = {
+        enabled             = true
+        interval            = 30
+        path                = "/app1/index.html"
+        port                = "traffic-port"
+        healthy_threshold   = 3
+        unhealthy_threshold = 3
+        timeout             = 5
+        protocol            = "HTTP"
+        matcher             = "200-399"
+      }
+
+      protocol_version = "HTTP1"
+      targets = {
+        my_app1_jass = {
+          target_id = module.ec2_private[0].id
+          port      = 80
+        }
+        my_app1_jass2 = {
+          target_id = module.ec2_private[1].id
+          port      = 8080
+        }
+      }
+      tags = local.common_tags
+    }
+  ]
+  tags = local.common_tags
+
+}
