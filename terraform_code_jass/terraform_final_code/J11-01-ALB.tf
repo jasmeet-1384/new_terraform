5de753cba350bd65913707fa46778a23b8d8dc1e
jass <your@email.com>
2023-06-16 16:07:47 +0530

    jass

--- terraform_code_jass/terraform_final_code/J11-01-ALB.tf
+++ terraform_code_jass/terraform_final_code/J11-01-ALB.tf
@@ -17,13 +17,13 @@ module "alb" {
   # Listeners
   http_tcp_listeners = [
     {
-      port               = 80
-      protocol           = "HTTP"
+      port     = 80
+      protocol = "HTTP"
       #target_group_index = 0
       action_type = "redirect"
       redirect = {
-        port = "443"
-        protocol = "HTTPS"
+        port        = "443"
+        protocol    = "HTTPS"
         status_code = "HTTP_301"
       }
     }
@@ -33,11 +33,11 @@ module "alb" {
   target_groups = [
     # App1 Target Group - TG Index = 0
     {
-      name_prefix      = "App1-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-deregistration_delay = 10
+      name_prefix          = "App1-"
+      backend_protocol     = "HTTP"
+      backend_port         = 80
+      target_type          = "instance"
+      deregistration_delay = 10
       health_check = {
         enabled             = true
         interval            = 30
@@ -64,11 +64,11 @@ deregistration_delay = 10
       }
       tags = local.common_tags
     },
-  
-  #tags = local.common_tags
 
-  # App2 Target Group - TG Index = 1
-{
+    #tags = local.common_tags
+
+    # App2 Target Group - TG Index = 1
+    {
       name_prefix          = "App2-"
       backend_protocol     = "HTTP"
       backend_port         = 80
@@ -97,48 +97,48 @@ deregistration_delay = 10
           port      = 80
         }
       }
-      tags =local.common_tags 
-    }  
+      tags = local.common_tags
+    }
   ]
 
-# HTTPS Listener
+  # HTTPS Listener
   https_listeners = [
     # HTTPS Listener Index = 0 for HTTPS 443
     {
-      port               = 443
-      protocol           = "HTTPS"
-      certificate_arn    = module.acm.acm_certificate_arn
-      action_type = "fixed-response"
+      port            = 443
+      protocol        = "HTTPS"
+      certificate_arn = module.acm.acm_certificate_arn
+      action_type     = "fixed-response"
       fixed_response = {
         content_type = "text/plain"
         message_body = "Fixed Static message - for Root Context"
         status_code  = "200"
       }
-    }, 
+    },
   ]
 
   # HTTPS Listener Rules
   https_listener_rules = [
-    # Rule-1: /app1* should go to App1 EC2 Instances
-    { 
-      https_listener_index = 0
+    # Rule-1: /app1*
+    {
+      #https_listener_index = 0
       actions = [
         {
           type               = "forward"
-          target_group_index = 0
+          #target_group_index = 0
         }
       ]
       conditions = [{
         path_patterns = ["/app1*"]
       }]
     },
-# Rule-2: /app2* should go to App2 EC2 Instances
- { 
-      https_listener_index = 1
+    # Rule-2: /app2* 
+    {
+     # https_listener_index = 1
       actions = [
         {
           type               = "forward"
-          target_group_index = 1
+         #target_group_index = 1
         }
       ]
       conditions = [{
