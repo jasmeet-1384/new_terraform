caaf8ccea86cb69ddbe9a6161386251507829a9a
jass <your@email.com>
2023-06-16 15:27:00 +0530

    jass

--- terraform_code_jass/terraform_final_code/J7-02-ec2instance-outputs.tf
+++ terraform_code_jass/terraform_final_code/J7-02-ec2instance-outputs.tf
@@ -19,14 +19,27 @@ output "ec2_bastion_public_ip" {
 output "ec2_private_instance_ids" {
   description = "List of IDs of instances"
   #value       = [module.ec2_private.id]
-  value = [for ec2private in module.ec2_private: ec2private.id ]   
+  value = [for ec2private in module.ec2_private_app1: ec2private.id ]   
 }
 
 ## ec2_private_ip
 output "ec2_private_ip" {
   description = "List of private IP addresses assigned to the instances"
   #value       = [module.ec2_private.private_ip]
-  value = [for ec2private in module.ec2_private: ec2private.private_ip ]  
+  value = [for ec2private in module.ec2_private_app1: ec2private.private_ip ]  
+}
+
+output "ec2_private_instance_ids" {
+  description = "List of IDs of instances"
+  #value       = [module.ec2_private.id]
+  value = [for ec2private in module.ec2_private_app2: ec2private.id ]   
+}
+
+## ec2_private_ip
+output "ec2_private_ip" {
+  description = "List of private IP addresses assigned to the instances"
+  #value       = [module.ec2_private.private_ip]
+  value = [for ec2private in module.ec2_private_app2: ec2private.private_ip ]  
 }
 
 
